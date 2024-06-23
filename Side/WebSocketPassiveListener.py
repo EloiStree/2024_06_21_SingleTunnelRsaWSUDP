45252f2b0d1daa0eac32d78570658f4ea8c63e79
Eloi Stree <eloistree@gmail.com>
2024-06-24 00:25:36 +0200

    save

---
+++ Side/WebSocketPassiveListener.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+import time
+
+async def connect_to_websocket():
+    uri = "ws://81.240.94.97:4504"  # Replace with your WebSocket server URI
+
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                while True:
+                    message = await websocket.recv()
+                    if(message is not None and message != "" and len(message) > 0):
+                        if isinstance(message, str):
+                            print(f"Received text message: {message}")
+                        else:
+                            print(f"Received non-text message.{len(message)}")
+        except websockets.exceptions.ConnectionClosedError as e:
+            print(f"Connection closed with exception: {e}")
+        except Exception as e:
+            print(f"Exception: {e}")
+        time.sleep(5)
+        
+
+asyncio.get_event_loop().run_until_complete(connect_to_websocket())
